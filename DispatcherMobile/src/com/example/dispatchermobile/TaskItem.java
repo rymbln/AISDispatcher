8d44ef9e69764aa681922558b334196faa877a6c
rymbln <rymbln@gmail.com>
2013-09-19 17:36:09 +0400

    First working Copy
    + Loading Tasks from GoogleApp

--- DispatcherMobile/src/com/example/dispatchermobile/TaskItem.java
+++ DispatcherMobile/src/com/example/dispatchermobile/TaskItem.java
@@ -1,15 +1,8 @@
 package com.example.dispatchermobile;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
-//import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-//import java.util.Date;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -86,30 +79,12 @@ public class TaskItem
 		
 		try
 		{
-			//opening localtest file
 			   			
 			// opening connection for downloading list of tasks by xml data
 			URL _url = new URL(_sourceUrl);
-//			HttpURLConnection _conn = (HttpURLConnection)_url.openConnection();
-//			
-//			if (_conn.getResponseCode() == HttpURLConnection.HTTP_OK)
-//			{
-//				InputStream _is = _conn.getInputStream();
-//				
-//				// Create DocumentBuilder for parsing xml data
-//				DocumentBuilderFactory _dbf = DocumentBuilderFactory.newInstance();
-//				DocumentBuilder _db = _dbf.newDocumentBuilder();
-//				
-//				// Parsing
-//				Document _doc = _db.parse(_is);
-//				*/
-				//Element _el = _doc.getDocumentElement(); 
-				
-				// take nodes to nodeList
-				//NodeList _nl = _el.getElementsByTagName("item");
 				
 			URLConnection _conn = _url.openConnection();
-
+			_conn.setRequestProperty("Accept", "application/xml");
             DocumentBuilderFactory _factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder _builder = _factory.newDocumentBuilder();
             Document _doc = _builder.parse(_conn.getInputStream());
@@ -123,21 +98,13 @@ public class TaskItem
 						//reading tags for each task item between tags <item></item>
 						Element _item = (Element) _nl.item(i);
 						
-						Element _companyName = (Element) _item.getElementsByTagName("CompanyName").item(0);
-						Element _deliveryTime = (Element) _item.getElementsByTagName("DeliveryTime").item(0);
-						Element _address = (Element) _item.getElementsByTagName("Address");
-						Element _comment = (Element) _item.getElementsByTagName("Comment");
-						Element _lastStatus = (Element) _item.getElementsByTagName("LastStatus");
-						Element _lastStatusDate = (Element) _item.getElementsByTagName("LastStatusDate");
-						Element _driverName = (Element) _item.getElementsByTagName("DriverName");
-						
-						String _cn = _companyName.getFirstChild().getNodeValue();
-						String _dt = _deliveryTime.getFirstChild().getNodeValue();
-						String _ad = _address.getFirstChild().getNodeValue();
-						String _cm = _comment.getFirstChild().getNodeValue();
-						String _ls = _lastStatus.getFirstChild().getNodeValue();
-						String _ld = _lastStatusDate.getFirstChild().getNodeValue();
-						String _dn = _driverName.getFirstChild().getNodeValue();
+						String _cn = ((Element) _item.getElementsByTagName("CompanyName").item(0)).getFirstChild().getNodeValue();
+						String _dt = ((Element) _item.getElementsByTagName("DeliveryTime").item(0)).getFirstChild().getNodeValue();
+						String _ad = ((Element) _item.getElementsByTagName("Address").item(0)).getFirstChild().getNodeValue();
+						String _cm = ((Element) _item.getElementsByTagName("Comment").item(0)).getFirstChild().getNodeValue();
+						String _ls = ((Element) _item.getElementsByTagName("LastStatus").item(0)).getFirstChild().getNodeValue();
+						String _ld = ((Element) _item.getElementsByTagName("LastStatusDate").item(0)).getFirstChild().getNodeValue();
+						String _dn = ((Element) _item.getElementsByTagName("DriverName").item(0)).getFirstChild().getNodeValue();
 						
 						// Create TaskItem object and add to ArrayList
 						TaskItem _taskItem = new TaskItem(_cn, _dt, _ad, _cm, _ls, _ld, _dn);
--- DispatcherMobile/src/com/example/dispatchermobile/TaskItemSelected.java
+++ DispatcherMobile/src/com/example/dispatchermobile/TaskItemSelected.java
@@ -1,7 +1,5 @@
 package com.example.dispatchermobile;
 
-import java.text.SimpleDateFormat;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -23,23 +21,14 @@ public class TaskItemSelected extends Activity
 		TextView _lastStatusDateTV = (TextView)findViewById(R.id.lastStatusDateTextView);
 		TextView _driverNameTV = (TextView)findViewById(R.id.driverNameTextView);
 		
-		SimpleDateFormat _sdf = new SimpleDateFormat("hh:mm:ss");
-		
-		String _cn = _selectedTaskItem.getCompanyName();
-		String _dt = _sdf.format(_selectedTaskItem.getDeliveryTime());
-		String _ad = _selectedTaskItem.getDeliveryTime();
-		String _cm = _selectedTaskItem.getComment();
-		String _ls = _selectedTaskItem.getLastStatus();
-		String _ld = _selectedTaskItem.getLastStatusDate();
-		String _dn = _selectedTaskItem.getDriverName();
-		
-		_companyNameTV.setText(_cn);
-		_deliveryTimeTV.setText(_dt);
-		_addressTV.setText(_ad);
-		_commentTV.setText(_cm);
-		_lastStatusTV.setText(_ls);
-		_lastStatusDateTV.setText(_ld);
-		_driverNameTV.setText(_dn);
+		_companyNameTV.setText(_selectedTaskItem.getCompanyName());
+		_deliveryTimeTV.setText(_selectedTaskItem.getDeliveryTime());
+		_addressTV.setText(_selectedTaskItem.getAddress());
+		_commentTV.setText(_selectedTaskItem.getComment());
+		_lastStatusTV.setText(_selectedTaskItem.getLastStatus());
+		_lastStatusDateTV.setText(_selectedTaskItem.getLastStatusDate());
+		_driverNameTV.setText(_selectedTaskItem.getDriverName());
 		
 	}
+	
 }
